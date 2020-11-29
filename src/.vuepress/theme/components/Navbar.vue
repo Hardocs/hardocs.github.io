22066d0434acbc2f6220747ad24ce573e06dd139
Jose Carlos Urra <jose.urra86@gmail.com>
2020-11-29 16:51:08 +0100

    debugging broken build

--- src/.vuepress/theme/components/Navbar.vue
+++ src/.vuepress/theme/components/Navbar.vue
@@ -30,7 +30,7 @@
       <NavLinks class="can-hide"/>
 
       <!-- Theme Switcher -->
-      <!-- <button
+      <button
         role="button"
         aria-label="Toggle dark/light"
         @click.prevent="toggleTheme"
@@ -74,7 +74,7 @@
         >
           <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
         </svg>
-      </button> -->
+      </button>
       <!-- End Theme Switcher -->
       
     </div>
